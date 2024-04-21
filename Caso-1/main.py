f3db67a409770307b3a5a69d935ea4c922d0a892
Ignacio687 <ignadavidchaves@gmail.com>
2024-04-21 00:29:26 -0300

    Caso-1 done

--- Caso-1/main.py
+++ Caso-1/main.py
@@ -1,54 +1,73 @@
 #!/usr/bin/env python
 
 from mininet.net import Mininet
-from mininet.topo import Topo
-from mininet.node import OVSSwitch
-from mininet.log import setLogLevel
-from mininet.cli import CLI
 from mininet.node import Node
+from mininet.node import OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
 import random
 
 class CustomRouter(Node):
     def config(self, **params):
         super(CustomRouter, self).config(**params)
-        # Habilitar el reenvío de IP
-        self.cmd('sysctl net.ipv4.ip_forward=1')
 
     def terminate(self):
-        self.cmd('sysctl net.ipv4.ip_forward=0')
         super(CustomRouter, self).terminate()
 
-class CustomTopology(Topo):
+def myNetwork():
 
-    def build(self):
-        rootNetRouter = self.addNode('rootNetRouter', cls=CustomRouter, ip='192.168.100.6')
-        for branchCounter in range(1, 7):
-            rootNetSwitch = self.addSwitch('rootNetSwitch{}'.format(branchCounter))
-            self.addLink(rootNetRouter, rootNetSwitch)
-            branchRouter = self.addNode('branch'+str(branchCounter)+'Router', cls=CustomRouter, ip='10.0.{}.1/29'.format(branchCounter))
-            branchSwitch = self.addSwitch('branch'+str(branchCounter)+'Switch')
-            self.addLink(branchRouter, rootNetSwitch)
-            self.addLink(branchRouter, branchSwitch)
-            for hostCounter in range(0, random.randint(3, 15)):
-                host = self.addHost('host'+str(hostCounter)+'Branch'+str(branchCounter))
-                self.addLink(host, branchSwitch)
+    net = Mininet( topo=None,
+                   build=False)
 
-    @staticmethod
-    def routing(net):
-        rootNetRouter = net.get('rootNetRouter')
-        for netInterfaceCounter in range(0, 6):
-            if netInterfaceCounter < 5:
-                rootNetRouter.cmd('ip addr add 192.168.100.{}/29 dev eth{}'.format(14+(8*netInterfaceCounter), netInterfaceCounter+1))
-            rootNetRouter.cmd('ip route add 10.0.{}.0/24 via 192.168.100.{}'.format(netInterfaceCounter+1, 1+(8*netInterfaceCounter)))
-            branchRouter = net.get('branch'+str(netInterfaceCounter+1)+'Router')
-            branchRouter.cmd('ip addr add 192.168.100.{}/29 dev eth{}'.format(1+(8*netInterfaceCounter), netInterfaceCounter+1))
-            branchRouter.cmd('ip route add 10.0.0.0/21 via 192.168.100.{}'.format(6+(8*netInterfaceCounter)))
-            
+    mainNetRouter = net.addHost('mr', cls=CustomRouter)
+    for branchCounter in range(1, 7):
+        mainNetSwitch = net.addSwitch('ms{}'.format(branchCounter), cls=OVSKernelSwitch, 
+                                    failMode='standalone')
+        net.addLink(mainNetRouter, mainNetSwitch)
+        branchRouter = net.addHost('b{}r'.format(branchCounter), cls=CustomRouter, 
+                                    ip='10.0.{}.1/24'.format(branchCounter))
+        branchSwitch = net.addSwitch('b{}s'.format(branchCounter), cls=OVSKernelSwitch, 
+                                    failMode='standalone')
+        net.addLink(branchRouter, branchSwitch)
+        net.addLink(branchRouter, mainNetSwitch)
+        info( '*** Add hosts\n')
+        for hostCounter in range(1, random.randint(3, 15)):
+            host = net.addHost('h{}b{}'.format(hostCounter, branchCounter), 
+                                ip='10.0.{}.{}/24'.format(branchCounter, hostCounter+1))
+            net.addLink(host, branchSwitch)
 
-if __name__ == '__main__':
-    setLogLevel('info')
-    net = Mininet(topo=CustomTopology(), controller=None)
-    net.start()
-    CustomTopology.routing(net)
+    info( '*** Starting network\n')
+    net.build()
+
+    info( '*** Starting switches\n')
+    for switch in net.switches:
+        switch.start([])
+
+    info( '*** Post configure switches and hosts\n')
+    mainNetRouter = net.get('mr')
+    mainNetRouter.cmd('sysctl net.ipv4.ip_forward=1')
+    for netIntCounter in range(0, 6):
+        mainNetRouter.cmd('ip addr add 192.168.100.{}/29 dev mr-eth{}'
+                            .format(6+(8*(netIntCounter)), netIntCounter))
+        mainNetRouter.cmd('ip route add 10.0.{}.0/24 via 192.168.100.{}'
+                            .format(netIntCounter+1, 1+(8*netIntCounter)))
+        branchRouter = net.get('b'+str(netIntCounter+1)+'r')
+        branchRouter.cmd('ip addr add 192.168.100.{}/29 dev b{}r-eth1'
+                            .format(1+(8*(netIntCounter)), netIntCounter+1))
+        branchRouter.cmd('ip route add 10.0.0.0/21 via 192.168.100.{}'.format(6+(8*netIntCounter)))
+        branchRouter.cmd('sysctl net.ipv4.ip_forward=1')
+        hostCounter = 1
+        while True:
+            try:
+                host = net.get('h{}b{}'.format(hostCounter, netIntCounter+1))
+                host.cmd('ip route add default via 10.0.{}.1'.format(netIntCounter+1))
+                hostCounter += 1
+            except KeyError:
+                break
+
     CLI(net)
     net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
